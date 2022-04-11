6e0e0a5416be651b6684325c89a2e37a57bbecab
LEIZ3R <alixsonbastidas@gmail.com>
2022-04-11 00:43:29 -0300

    angular-basic-practice-v0.1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
 import { HerosModule } from './heroes/heroes.module';
-import { ContadorComponent } from './contador/contador/contador.component';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 
 @NgModule({
@@ -16,7 +14,8 @@ import { ContadorModule } from './contador/contador.module';
   imports: [
     BrowserModule,
     HerosModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
